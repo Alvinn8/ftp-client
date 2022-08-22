4fedc9ebc727ee0c3bf3c9e1d908ec5b1a0c6e8d
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2022-08-22 19:39:36 +0200

    Use bootstrap-dark for dark theme

--- src/common/ui/index.tsx
+++ src/common/ui/index.tsx
@@ -2,4 +2,18 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+async function loadBootstrap() {
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    // Typescript does not understand dynamic imports with css, so we use @ts-ignore
+    if (isDarkMode) {
+        // @ts-ignore
+        await import("@forevolve/bootstrap-dark/dist/css/bootstrap-dark.min.css");
+    } else {
+        // @ts-ignore
+        await import("bootstrap/dist/css/bootstrap.min.css");
+    }
+}
+
+loadBootstrap().then(() => {
+    ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+});
